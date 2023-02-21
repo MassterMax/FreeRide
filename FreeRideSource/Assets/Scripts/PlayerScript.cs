5d1368e5e3b61d2c8744b9a1565cbf86087ae630
MassterMax <59767630+MassterMax@users.noreply.github.com>
2023-02-21 23:41:34 +0300

    add cinemachine camera

--- FreeRideSource/Assets/Scripts/PlayerScript.cs
+++ FreeRideSource/Assets/Scripts/PlayerScript.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] GameObject board;
-    [SerializeField] float acceleration = 1f;
+    [SerializeField] float acceleration = 10f;
     [SerializeField] float xSpeed = 1f;
 
-    float currentSpeed = 0f;
+    //todo remove it!
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
+
+    public float currentSpeed = 0f;
+    float minSpeed = 0.1f;
 
     // [SerializeField] 
     float angleRotationSpeed = 100f;
 
     public float angle = 0f;
 
+    float minOrthographicSize = 5f;
+    float maxOrthographicSize = 25f;
+
 
     void Start()
     {
-
+        virtualCamera.m_Lens.OrthographicSize = minOrthographicSize;
     }
 
     // Update is called once per frame
@@ -27,6 +35,7 @@ public class PlayerScript : MonoBehaviour
         UpdateAngle();
         RotateBoard();
         Move();
+        UpdateCamera();
     }
 
     void UpdateAngle()
@@ -63,18 +72,29 @@ public class PlayerScript : MonoBehaviour
         {
             currentSpeed = 0f;
         }
-        else if (Mathf.Abs(angle) > 45)
+        else if (Mathf.Abs(angle) > 60)
         {
-            // do not change speed or increase slightly after stop
-            if (currentSpeed == 0f)
-                currentSpeed = 1f;
+            // slow down
+            // todo change to slow down depend on angle
+            // float slowAcceleration = acceleration * 
+            currentSpeed = Mathf.Max(currentSpeed - Time.deltaTime * acceleration, minSpeed);
+        }
+        else if (Mathf.Abs(angle) > 30)
+        {
+            // do not change speed
         }
         else
         {
             currentSpeed += Time.deltaTime * acceleration;
         }
 
-        Vector3 transaltion = new Vector3(Sign(angle) * xSpeed, -currentSpeed, 0) * Time.deltaTime;
+        Vector3 transaltion = new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle * 2) * currentSpeed, -currentSpeed, 0) * Time.deltaTime;
         transform.Translate(transaltion, Space.World);
+    }
+
+    void UpdateCamera()
+    {
+        //todo move to separate script
+        virtualCamera.m_Lens.OrthographicSize = minOrthographicSize + currentSpeed / 4;
     }
 }
